666a4fe676207395898051fbdd2d2875ea94447b
Olivier Truong <olivier.truong@momentslab.com>
2024-10-17 07:58:21 +0200

    First draft

---
+++ src/main/java/org/acme/CustomGreetingResource.java
@@ -0,0 +1,20 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/customhello")
+public class CustomGreetingResource {
+
+    @ConfigProperty(name = "RUNNING_ENV", defaultValue = "UNKOWN RUNNING_ENV")
+    String environment;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST, running from environment " + environment;
+    }
+}
